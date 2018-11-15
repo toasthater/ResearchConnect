96652118554c33a08a408a721896ff094c92d6c5
Daniel Williams <danswilliams95@gmail.com>
2018-11-14 16:17:05 -0800

    setting up tables

--- models/tables.py
+++ models/tables.py
@@ -36,3 +36,39 @@ db.define_table('thumb',
                 Field('post_id', 'reference post'), # The thumbed post
                 Field('thumb_state'), # This can be 'u' for up or 'd' for down, or None for... None.
                 )
+
+# Students
+db.define_table('student',
+                Field('student_name'),
+                Field('student_major'),
+                Field('student_resume'),#file upload
+                Field('student_bio'),
+                Field('student_skills'),#list
+                Field('student_interest'),#list
+                Field('student_following'),#list
+                Field('student_follower'),#list 
+                Field('student_accept_state')
+)
+
+
+# Professors
+db.define_table('professor',
+                Field('prof_name'),
+                Field('prof_dep'),
+                Field('prof_email',default=get_user_email()),
+                Field('prof_hours'),#list
+                Field('prof_classes'),#list
+                Field('prof_research'),#list
+                Field('prof_following'),#list
+                Field('prof_follower'),#list
+                )
+# Research Oppurtunity
+db.define_table('research_post',
+                Field('research_topic'),
+                Field('research_professor'),
+                Field('research_major'),
+                Field('research_req'),#list
+                Field('research_tags'),#list
+                Field('research_students'),#list of student objs
+)
+# User
