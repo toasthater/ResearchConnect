36d295d486bb0741a55cd133a0c617ea6d056839
Giulio Colleluori <gcollelu@ucsc.edu>
2018-11-17 13:08:54 -0800

    fixed home button redirection

--- models/menu.py
+++ models/menu.py
@@ -29,7 +29,7 @@ response.google_analytics_id = None
 # ----------------------------------------------------------------------------------------------------------------------
 
 response.menu = [
-    (T('Home'), False, URL('default', 'index'), [])
+    (T('Home'), False, URL('main'), [])
 ]
 
 DEVELOPMENT_MENU = True
