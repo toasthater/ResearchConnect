dd97b769e7376e6e719c2c1f7c414efbb1e09363
Abdo <abdurrahman.alomari@gmail.com>
2018-11-29 20:16:06 -0800

    Fixed getpostlist

--- controllers/api.py
+++ controllers/api.py
@@ -50,8 +50,8 @@ def get_post_list():
                 post_title=row.post_title,
                 post_content=row.post_content,
                 post_author=row.post_author,
-                post_department=row.post_department,
-                post_tags=row.post_tags,
+                post_department=row.post.post_department,
+                post_tags=row.post.post_tags,
                 thumb = None,
                 score=0
             ))
@@ -71,8 +71,8 @@ def get_post_list():
                 post_title=row.post.post_title,
                 post_content=row.post.post_content,
                 post_author=row.post.post_author,
-                post_department=row.post_department,
-                post_tags=row.post_tags,
+                post_department=row.post.post_department,
+                post_tags=row.post.post_tags,
                 thumb = None if row.thumb.id is None else row.thumb.thumb_state,
                 score=post_score
             ))
