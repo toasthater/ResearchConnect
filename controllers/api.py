0f9682b2f087cde81e46641197746857148d5f65
Abdurrahman Al-Omari <abdurrahman.alomari@gmail.com>
2018-12-11 17:01:27 -0800

    small change

--- controllers/api.py
+++ controllers/api.py
@@ -353,6 +353,7 @@ def get_participants():
                 id = participant.id,
                 name = participant.name,
                 email = participant.email,
+                user_id = applicant.user_id
             ))
     return response.json(dict(participant_list = participant_list))
     
